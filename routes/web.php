5c351e3fc5fdf3d2bb666a2e0aee1f9b95447501
Jose Sanchez <alejo3261@hotmail.com>
2022-03-30 16:51:26 -0500

    Changes in the web

--- routes/web.php
+++ routes/web.php
@@ -19,10 +19,6 @@ Route::get('/products', 'App\Http\Controllers\ProductController@index')->name("p
 Route::get('/products/create', 'App\Http\Controllers\ProductController@create')->name("product.create");
 Route::post('/products/save', 'App\Http\Controllers\ProductController@save')->name("product.save");
 Route::get('/products/{id}', 'App\Http\Controllers\ProductController@show')->name("product.show");
-/*Route::get('/image', 'App\Http\Controllers\ImageController@index')->name("image.index");
-Route::post('/image/save', 'App\Http\Controllers\ImageController@save')->name("image.save");
-Route::get('/image-not-di', 'App\Http\Controllers\ImageNotDIController@index')->name("imagenotdi.index");
-Route::post('/image-not-di/save', 'App\Http\Controllers\ImageNotDIController@save')->name("imagenotdi.save");*/
 Route::get('/cart', 'App\Http\Controllers\CartController@index')->name("cart.index");
 Route::get('/cart/delete', 'App\Http\Controllers\CartController@delete')->name("cart.delete");
 Route::post('/cart/add/{id}', 'App\Http\Controllers\CartController@add')->name("cart.add");
@@ -39,21 +35,21 @@ Route::middleware('admin')->group(function () {
     Route::get('/admin/products/search', 'App\Http\Controllers\Admin\AdminProductController@search')->name("admin.product.search");
     Route::get('/admin/products/create', 'App\Http\Controllers\Admin\AdminProductController@create')->name("admin.product.create");
     Route::post('/admin/products/store', 'App\Http\Controllers\Admin\AdminProductController@store')->name("admin.product.store");
-    Route::delete('/admin/products/{id}/delete', 'App\Http\Controllers\Admin\AdminProductController@delete')->name("admin.product.delete");
+    Route::delete('/admin/products/{id}/delete', 'App\Http\Controllers\Admin\AdminProductController@destroy')->name("admin.product.delete");
     Route::get('/admin/products/{id}/edit', 'App\Http\Controllers\Admin\AdminProductController@edit')->name("admin.product.edit");
     Route::put('/admin/products/{id}/update', 'App\Http\Controllers\Admin\AdminProductController@update')->name("admin.product.update");
     Route::get('/admin/categories', 'App\Http\Controllers\Admin\AdminCategoryController@index')->name("admin.category.index");
     Route::get('/admin/categories/search', 'App\Http\Controllers\Admin\AdminCategoryController@search')->name("admin.category.search");
     Route::get('/admin/categories/create', 'App\Http\Controllers\Admin\AdminCategoryController@create')->name("admin.category.create");
     Route::post('/admin/categories/store', 'App\Http\Controllers\Admin\AdminCategoryController@store')->name("admin.category.store");
-    Route::delete('/admin/categories/{id}/delete', 'App\Http\Controllers\Admin\AdminCategoryController@delete')->name("admin.category.delete");
+    Route::delete('/admin/categories/{id}/delete', 'App\Http\Controllers\Admin\AdminCategoryController@destroy')->name("admin.category.delete");
     Route::get('/admin/categories/{id}/edit', 'App\Http\Controllers\Admin\AdminCategoryController@edit')->name("admin.category.edit");
     Route::put('/admin/categories/{id}/update', 'App\Http\Controllers\Admin\AdminCategoryController@update')->name("admin.category.update");
     Route::get('/admin/goals', 'App\Http\Controllers\Admin\AdminGoalController@index')->name("admin.goal.index");
     Route::get('/admin/goals/search', 'App\Http\Controllers\Admin\AdminGoalController@search')->name("admin.goal.search");
     Route::get('/admin/goals/create', 'App\Http\Controllers\Admin\AdminGoalController@create')->name("admin.goal.create");
     Route::post('/admin/goals/store', 'App\Http\Controllers\Admin\AdminGoalController@store')->name("admin.goal.store");
-    Route::delete('/admin/goals/{id}/delete', 'App\Http\Controllers\Admin\AdminGoalController@delete')->name("admin.goal.delete");
+    Route::delete('/admin/goals/{id}/delete', 'App\Http\Controllers\Admin\AdminGoalController@destroy')->name("admin.goal.delete");
     Route::get('/admin/goals/{id}/edit', 'App\Http\Controllers\Admin\AdminGoalController@edit')->name("admin.goal.edit");
     Route::put('/admin/goals/{id}/update', 'App\Http\Controllers\Admin\AdminGoalController@update')->name("admin.goal.update");
 });
