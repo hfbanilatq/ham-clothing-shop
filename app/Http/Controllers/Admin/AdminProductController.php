52abd585f665ad3d1b8c07f4ebf7396d5321d6cc
Hector Fabio Banilat Quintero <53194376+hfbanilatq@users.noreply.github.com>
2022-03-30 11:02:09 -0500

    solving clicts with import in product controller

--- app/Http/Controllers/Admin/AdminProductController.php
+++ app/Http/Controllers/Admin/AdminProductController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Interfaces\ImageStorage;
 use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Http\Request;
--- app/Http/Controllers/DiscountController.php
+++ app/Http/Controllers/DiscountController.php
@@ -7,15 +7,7 @@ use Illuminate\Http\Request;
 
 class DiscountController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
+
 
     /**
      * Show the form for creating a new resource.
